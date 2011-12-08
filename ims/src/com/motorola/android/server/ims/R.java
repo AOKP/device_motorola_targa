584108ae4c5e53a0d312a74137e72cfd8b45a85a
David Hacker <davidhackerdvm@gmail.com>
2011-12-08 17:35:42 -0600

    add ims server app

---
+++ ims/src/com/motorola/android/server/ims/R.java
@@ -0,0 +1,22 @@
+package com.motorola.android.server.ims;
+
+public final class R
+{
+  public static final class string
+  {
+    public static final int content_title = 2130837507;
+    public static final int context_text = 2130837508;
+    public static final int ims_403_dialog = 2130837504;
+    public static final int ims_403_dialog_activity_text = 2130837505;
+    public static final int ticker_text = 2130837506;
+  }
+
+  public static final class attr
+  {
+  }
+}
+
+/* Location:           /home/dhacker29/jd/classes_dex2jar.jar
+ * Qualified Name:     com.motorola.android.server.ims.R
+ * JD-Core Version:    0.6.0
+ */
