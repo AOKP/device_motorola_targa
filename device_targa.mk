11d622c1cbf37c6ca4291da8dd895a22eab74226
dhacker29 <davidhackerdvm@gmail.com>
2011-12-16 06:10:47 -0600

    typo

--- device_targa.mk
+++ device_targa.mk
@@ -132,7 +132,7 @@ PRODUCT_COPY_FILES += \
     device/motorola/targa/root/init.rc:system/etc/rootfs/init.rc \
     device/motorola/targa/root/init.mapphone_cdma.rc:system/etc/rootfs/init.mapphone_cdma.rc \
     device/motorola/targa/root/init.mapphone_umts.rc:system/etc/rootfs/init.mapphone_umts.rc \
-    device/motorola/targa/root/init.maphone.usb.rc:system/etc/rootfs/init.maphone.usb.rc \
+    device/motorola/targa/root/init.mapphone.usb.rc:system/etc/rootfs/init.mapphone.usb.rc \
     device/motorola/targa/root/usbcheck.sh:system/etc/rootfs/usbcheck.sh \
     device/motorola/targa/root/ueventd.rc:system/etc/rootfs/ueventd.rc \
 
