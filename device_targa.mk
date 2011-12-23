b3089e05ec4118ed311b6ab7103ffe2d3eed1b9f
dhacker29 <davidhackerdvm@gmail.com>
2011-12-22 20:46:25 -0600

    remove whisper and reference one in Stingray

--- device_targa.mk
+++ device_targa.mk
@@ -241,8 +241,9 @@ $(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
 $(call inherit-product-if-exists, vendor/google/google-vendor.mk)
 #$(call inherit-product, hardware/ti/wpan/tools/FM/Android.mk)
 $(call inherit-product-if-exists, vendor/verizon/verizon-vendor.mk)
+$(call inherit-product-if-exists, vendor/verizon/verizon-vendor.mk)
 
-$(call inherit-product-if-exists, vendor/motorola/targa/targa-vendor.mk)
+$(call inherit-product-if-exists, device/moto/stingray/whisper/Android.mk)
 
 
 # stuff common to all Motorola phones -- disabled for Sandbox
