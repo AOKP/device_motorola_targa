5f6404d004388eac6731ba359f1bf5b1eaeef5f5
dhacker29 <davidhackerdvm@gmail.com>
2012-01-07 20:44:39 -0600

    switch to apn ver 6

--- device_targa.mk
+++ device_targa.mk
@@ -202,7 +202,7 @@ PRODUCT_COPY_FILES += \
 
 # Phone settings
 PRODUCT_COPY_FILES += \
-    device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml \
+    device/motorola/targa/prebuilt//etc/apns-conf.xml:system/etc/apns-conf.xml \
     vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml \
 
 
