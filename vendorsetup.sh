aea1f0c72eb579e8692538600200279d0fc060ce
David Hacker <davidhackerdvm@gmail.com>
2012-09-07 22:08:42 -0500

    CM10 board cleanup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_targa-userdebug
-add_lunch_combo cm_targa-userdebug
 add_lunch_combo aokp_targa-userdebug
+add_lunch_combo cm_targa-userdebug
+add_lunch_combo full_targa-userdebug
