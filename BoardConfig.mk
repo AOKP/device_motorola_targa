ecff81b14a6346a9d26615f0bc2fba285fd3b32d
dhacker29 <davidhackerdvm@gmail.com>
2012-09-09 12:15:08 -0500

    Use JB defconfig with OMAPLFB off

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ BOARD_PAGE_SIZE := 0x4096
 
 # Kernel Build
 TARGET_KERNEL_SOURCE := kernel/motorola/mapphone
-TARGET_KERNEL_CONFIG := mapphone_targa_defconfig
+TARGET_KERNEL_CONFIG := mapphone_targa_jb_defconfig
 
 # Recovery
 BOARD_NONSAFE_SYSTEM_DEVICE := /dev/block/mmcblk1p21
