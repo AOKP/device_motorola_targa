028da634340ca86b052edb4862f230635419beba
dhacker29 <davidhackerdvm@gmail.com>
2013-03-09 16:21:02 -0600

    Setup for kexec
    Change-Id: Icefa7bc8f8eaf4cc77feeca12adcdde258479114

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,3 +33,7 @@ TARGET_BOOTLOADER_BOARD_NAME := targa
 BOARD_KERNEL_CMDLINE := omap_wdt.timer_margin=60 oops=panic console=/dev/null rw mem=1048572K@0x80000000 vram=10300K omapfb.vram=0:8256K,1:4K,2:2040K init=/init ip=off mmcparts=mmcblk1:p7(pds),p15(boot),p16(recovery),p17(cdrom),p18(misc),p19(cid),p20(kpanic),p21(system),p22(cache),p23(preinstall),p24(webtop),p25(userdata),p26(emstorage) androidboot.bootloader=0x0A72
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_PAGE_SIZE := 0x4096
+
+# Kernel Build
+TARGET_KERNEL_SOURCE := kernel/motorola/omap4-common
+TARGET_KERNEL_CONFIG := mapphone_targa_defconfig
