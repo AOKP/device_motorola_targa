15d09d6abda710a74769f6880d35436e81ae0440
dhacker29 <davidhackerdvm@gmail.com>
2013-08-13 21:09:06 -0500

    Set device specific bluetooth name
    Change-Id: Id63cfd49e579a9848a3fe3901ebc6958720a5a58

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,9 @@
 # Processor
 TARGET_BOOTLOADER_BOARD_NAME := targa
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/motorola/targa/bluetooth
+
 # Kernel
 BOARD_KERNEL_CMDLINE := root=/dev/ram0 rw mem=1023M@0x80000000 console=null vram=10300K omapfb.vram=0:8256K,1:4K,2:2040K init=/init ip=off mmcparts=mmcblk1:p7(pds),p15(boot),p16(recovery),p17(cdrom),p18(misc),p19(cid),p20(kpanic),p21(system),p22(cache),p23(preinstall),p24(webtop),p25(userdata),p26(emstorage) mot_sst=1 androidboot.bootloader=0x0A72
 BOARD_KERNEL_BASE := 0x80000000
